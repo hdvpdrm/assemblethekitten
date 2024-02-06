f13a11fa3e60606f3e95c78d8b896848e37d2cab
MAGANER <maganer.krik@mail.ru>
2024-02-06 20:51:00 +0300

    added state

--- main.c
+++ main.c
@@ -1,21 +1,36 @@
 #include"start_menu.h"
 #include<stdio.h>
+
+
+typedef enum GameState {Start, Game, Death, Victory} GameState;
+
+void draw(GameState* state);
 int main(int argc, char** argv)
 {
   InitWindow(640,480,"WORLDOFFUCKINGSORROW");
   ClearWindowState(FLAG_WINDOW_RESIZABLE);
   SetTargetFPS(10);
+
+
+  GameState state = Start;
   
   while(!WindowShouldClose())
     {
-
-
       BeginDrawing();
       ClearBackground(BLACK);
-      draw_start_menu();
+      draw(&state);
       EndDrawing();
     }
 
   EndDrawing();
   return 0;
+}
+void draw(GameState* state)
+{
+  switch(*state)
+  {
+     case Start:
+       draw_start_menu();
+     break;
+  }
 }
