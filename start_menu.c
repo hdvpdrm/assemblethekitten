fd97885094a808e3b50a652abecfc4e862f6ab9c
MAGANER <maganer.krik@mail.ru>
2024-02-06 20:50:40 +0300

    module to draw/process start menu of the game

---
+++ start_menu.c
@@ -0,0 +1,7 @@
+#include"start_menu.h"
+
+
+void draw_start_menu(void)
+{
+  DrawText("WORLDOFFUCKINGSORROW",100,100,32,RAYWHITE);
+}
---
+++ start_menu.h
@@ -0,0 +1,9 @@
+#ifndef START_MENU_H
+#define START_MENU_H
+#include"raylib.h"
+
+extern void draw_start_menu(void);
+
+
+
+#endif
