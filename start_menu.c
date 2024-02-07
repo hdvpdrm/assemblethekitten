b578b2f7d6a7fc9cdc62bfd67b6cc48fcd1cab59
MAGANER <maganer.krik@mail.ru>
2024-02-07 16:28:32 +0300

    added text what actions can be performed

--- start_menu.c
+++ start_menu.c
@@ -4,4 +4,13 @@
 void draw_start_menu(void)
 {
   DrawText("WORLDOFFUCKINGSORROW",100,100,32,RAYWHITE);
+  DrawLine(98,130,538,130,RAYWHITE);
+
+  DrawText("Press",190,250,24,RAYWHITE);
+  DrawText("Q",285,250,24,MAGENTA);
+  DrawText("to start",320,250,24,RAYWHITE);
+
+  DrawText("Press",190,300,24,RAYWHITE);
+  DrawText("Z",285,300,24,MAGENTA);
+  DrawText("for help",320,300,24,RAYWHITE);
 }
