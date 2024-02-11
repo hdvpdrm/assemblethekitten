b51bb751452f75fcf8c9b38a3d535dd740499224
MAGANER <maganer.krik@mail.ru>
2024-02-11 14:08:18 +0300

    it works more predictable than raylib's one

---
+++ vector.c
@@ -0,0 +1,8 @@
+#include"vector.h"
+
+
+void vector_init(Vector* vec)
+{
+  vec->x = 0;
+  vec->y = 0;
+}
---
+++ vector.h
@@ -0,0 +1,10 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct
+{
+  int x, y;
+} Vector;
+extern void vector_init(Vector* vec);
+
+#endif
