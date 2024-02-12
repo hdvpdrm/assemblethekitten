b9d2ec5a00702ce34b8bc610659fe4009aa638ef
MAGANER <maganer.krik@mail.ru>
2024-02-12 17:15:42 +0300

    added method to generate random objects

--- map.c
+++ map.c
@@ -14,7 +14,93 @@ int compute_free_space()
   int S = 0;
   for(int y = 0;y<6;++y)
     for(int x = 0;x<6;++x)      
-	if(map[y][x])++S;
+      if(map[y][x] > -1)++S;
       
   return S;
+}
+int compute_free_cells(Vector** cells, int* cells_number)
+{
+  //create initial array for cells
+  *cells_number = 1;
+  *cells = malloc(sizeof(Vector));
+  if(*cells == NULL)
+    {
+      return -1;
+    }
+  (*cells)[0].x = -1; //-1 indicates that this element isn't filled with actual value
+  
+  for(int y = 0;y<6;++y)
+    for(int x = 0;x<6;++x)
+      {
+	//if it's under 0, then it's blocked,otherwise it's object
+	if(map[y][x] == 0)
+	  {
+	    //if first element isn't used, then fill it
+	    if( (*cells)[0].x == -1)
+	      {
+		(*cells)[0].x = x;
+		(*cells)[0].y = y;
+	      }
+	    //increase size of array and fill it with new value
+	    else
+	      {
+		*cells = realloc(*cells,sizeof(Vector)* ((*cells_number) + 1));
+		if(*cells == NULL)
+		  {
+		    return -1;
+		  }
+		*cells_number = *cells_number+1;
+
+		int index = (*cells_number) - 1;
+		(*cells)[index].x = x;
+		(*cells)[index].y = y;
+	      }	    
+	  }
+      }
+  
+  return 0;
+}
+int generate_random_objects(int amount)
+{
+  Vector* free_cells = NULL;
+  int free_cells_size = 0;
+  if(compute_free_cells(&free_cells,&free_cells_size) == 0)
+    {
+      int* random_cells_ids = LoadRandomSequence(amount, 0, free_cells_size-1);
+      if(random_cells_ids == NULL) return -1;
+      
+      for(int i = 0;i<amount;++i)
+	{
+	  int index = random_cells_ids[i];
+	  if(index < 0) index*=-1;
+	  int x = free_cells[index].x;
+	  int y = free_cells[index].y;
+
+	  int rnd_ch_pos = GetRandomValue(0,CHARS_LEN-1);
+	  map[y][x] = (int)_CHARS[rnd_ch_pos];
+	}
+
+      free(free_cells);
+      UnloadRandomSequence(random_cells_ids);
+      return 0;
+    }
+  free(free_cells);
+  return -1;
+}
+void draw_objects()
+{
+  for(int y = 0; y<CELL_MAX;++y)
+    for(int x = 0;x<CELL_MAX;++x)
+      {
+	int x_pos = COMP_X_POS(x)+2;
+	int y_pos = COMP_Y_POS(y)-1;
+       
+	if(map[y][x] > 0)
+	  {
+	    char buffer[2] = "0\0";
+	    buffer[0] = (char)map[y][x];
+	    DrawText(buffer,x_pos,y_pos,40,MAGENTA);
+	  }
+      }
+
 }
--- map.h
+++ map.h
@@ -1,9 +1,26 @@
 #ifndef MAP_H
 #define MAP_H
 #include"raylib.h"
+#include"vector.h"
+#include"MACRO.h"
+#include<stdlib.h>
+#include<string.h>
+
+#define CHARS "$%&/03456789<>@ABCDEFGHKLMNOPQRSUVWXYZ^"
+#define CHARS_LEN strlen(CHARS)
+
+static const char* _CHARS = CHARS;
 
 static int map[6][6];
 
+static int left_border  = 0;
+static int right_border = 6;
+
 extern void init_map();
 extern int compute_free_space();
+
+extern int compute_free_cells(Vector** cells, int* cells_number);
+extern int generate_random_objects(int amount);
+
+extern void draw_objects();
 #endif
